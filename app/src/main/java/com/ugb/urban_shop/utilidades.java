1df62b9055f530ccec528a3d722d08b5e8ab600b
DiegoZpeda <enmanuelzpeda9@gmail.com>
2023-05-05 11:45:01 -0600

    Ultimos arreglos

--- app/src/main/java/com/ugb/urban_shop/utilidades.java
+++ app/src/main/java/com/ugb/urban_shop/utilidades.java
@@ -1,8 +1,8 @@
 package com.ugb.urban_shop;
 
 public class utilidades {
-    static String url_consulta= "http://192.168.100.6:5984/db_amigos/_design/amigos/_view/amigos";
-    static  String url_mto = "http://192.168.100.6:5984/db_amigos/";
+    static String url_consulta= "http://192.168.224.1:5984/db_amigos/_design/amigos/_view/amigos";
+    static  String url_mto = "http://192.168.224.1.6:5984/db_amigos/";
 
     public String generarIdUnico(){
         return java.util.UUID.randomUUID().toString();
